39edf1931b5cdce6b59ad6a4d952ea1d9a2e13af
trep-kalkyl <emil.appelkvist@gmail.com>
2025-06-03 12:51:41 +0200

    Update ChangeLogService.js

--- ChangeLogService.js
+++ ChangeLogService.js
@@ -1,40 +1,35 @@
 // ChangeLogService.js
 const ChangeLogService = (() => {
-  // Unik lagring för loggar
-  const __tabulatorChangeLog = {
+  const _log = {
     category: {}, // { [rowId]: [logEntry, ...] }
     quantity: {}, // { [parentId_taskId]: [logEntry, ...] }
   };
 
-  // Logga kategoriändring på modernivå
   function logCategoryChange(rowId, oldCategory, newCategory) {
-    if (!__tabulatorChangeLog.category[rowId]) __tabulatorChangeLog.category[rowId] = [];
-    __tabulatorChangeLog.category[rowId].push({
+    if (!_log.category[rowId]) _log.category[rowId] = [];
+    _log.category[rowId].push({
       oldCategory,
       newCategory,
       timestamp: new Date().toISOString(),
     });
   }
 
-  // Logga antaländring på subnivå
   function logQuantityChange(parentId, taskId, oldQuantity, newQuantity) {
     const key = `${parentId}_${taskId}`;
-    if (!__tabulatorChangeLog.quantity[key]) __tabulatorChangeLog.quantity[key] = [];
-    __tabulatorChangeLog.quantity[key].push({
+    if (!_log.quantity[key]) _log.quantity[key] = [];
+    _log.quantity[key].push({
       oldQuantity,
       newQuantity,
       timestamp: new Date().toISOString(),
     });
   }
 
-  // Hämta logg för kategoriändringar
   function getCategoryLog(rowId) {
-    return __tabulatorChangeLog.category[rowId] || [];
+    return _log.category[rowId] || [];
   }
 
-  // Hämta logg för antaländringar
   function getQuantityLog(parentId, taskId) {
-    return __tabulatorChangeLog.quantity[`${parentId}_${taskId}`] || [];
+    return _log.quantity[`${parentId}_${taskId}`] || [];
   }
 
   return {
